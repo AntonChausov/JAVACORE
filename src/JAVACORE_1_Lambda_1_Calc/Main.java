8cae02f1bfb843a13d73bafc5f34a5123d6beb23
AntonChausov <antonchausov@gmail.com>
2021-08-05 12:04:35 +0300

    bugFix Integer.MAX_VALUE

--- src/JAVACORE_1_Lambda_1_Calc/Main.java
+++ src/JAVACORE_1_Lambda_1_Calc/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1,1);
-        int c = calc.divide.apply(a, b); //деление на 0
+        int c = calc.divide.apply(a, b); //деление на 0, будем возвращать "бесконечность"
         calc.println.accept(c);
     }
 }
